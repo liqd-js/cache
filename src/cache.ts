f7b6429f2621fce05c828354c14475da95ec1aed
Tomáš Korenko <radixxko@gmail.com>
2024-04-09 13:37:13 +0200

    Update cache.ts

--- src/cache.ts
+++ src/cache.ts
@@ -238,16 +238,3 @@ export default class Cache<T>
         return score;
     }
 }
-
-function bytesToSize( bytes: number )
-{
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
-    if( bytes === 0 ){ return '0 Byte'; }
-
-    const i = Math.floor( Math.log( bytes ) / Math.log( 1024 ) );
-
-    return Math.round( bytes / Math.pow( 1024, i ) ) + ' ' + sizes[i];
-}
-
-setInterval(() => console.log( bytesToSize( process.memoryUsage.rss() )), 1000 );
